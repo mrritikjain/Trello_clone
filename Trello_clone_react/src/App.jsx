3138bf85863c75b573f77105c5fffa5a7b89c826
RITIK JAIN <72255272+mrritikjain@users.noreply.github.com>
2026-01-06 20:49:14 +0530

    Tailwind is setup with project

---
+++ Trello_clone_react/src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Header from './Components/Header'
+import Sidebar from './Components/Sidebar'
+import Main from './Components/Main'
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Sidebar />
+      <Main />
+     <h1 className="text-4xl text-red-500 font-bold">
+  Tailwind OK 🚀
+</h1>
+
+    </>
+  )
+}
+
+export default App
